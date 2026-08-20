dfa90b4cb894697755a98b1085ef2b772c811841
owynhsiao-star <Owyn.hsiao@ucas-edu.net>
2026-08-20 09:34:53 -0600

    Chararacter intro

---
+++ Character_intro.py
@@ -0,0 +1,6 @@
+# Owyn hsiao, Character introduction
+Name = "Larry"
+Age = "92"
+Job = "bartender"
+Origin = "Kentucky."
+print("Hi, my name is " +Name + ". I am " +Age + " years old. I work as a " +Job +", and I'm from " +Origin)
