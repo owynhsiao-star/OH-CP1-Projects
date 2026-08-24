d1c0df6174e668cea622a45af444b376c164e2b1
owynhsiao-star <Owyn.hsiao@ucas-edu.net>
2026-08-24 09:13:20 -0600

    Debugging assignment

---
+++ Debugging.py
@@ -0,0 +1,9 @@
+# owyn Hsiao, Fix the code assignment
+
+# First error, I fixed th misspelling of "name" inside the print
+# Second error, He forgot to close the print, so I added th second parenthesis needed
+
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
