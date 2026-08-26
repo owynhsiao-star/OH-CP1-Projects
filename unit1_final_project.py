674766426426fc22d730ce27609550bc09baada5
owynhsiao-star <Owyn.hsiao@ucas-edu.net>
2026-08-26 09:50:18 -0600

    Unit one final project assignment

---
+++ unit1_final_project.py
@@ -0,0 +1,8 @@
+#Owyn Hsiao, unit 1 final project assignment
+
+name = input ("What is you name: ")
+age = input ("How old are you: ")
+boardgame = input ("What is you favorite boardgame: ")
+color = input ("What is your favorite color: ")
+
+print ("Hello! My name is " +name + ", I'm " +age + ", my favorite board game is " +boardgame + " and my favorite color is " +color + ". Nice to meet you!")
