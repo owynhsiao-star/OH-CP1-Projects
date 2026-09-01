6b4f24bef5d3b430072b370d3f8eebf4197eb9a6
Owynhsiao-star <Owyn.hsiao@ucas-edu.net>
2026-09-01 16:05:26 -0600

    Average grade assignment

--- Average_grade.py
+++ Average_grade.py
@@ -1,11 +1,13 @@
-try:
-    bio = int(input ("What is your biology grade?"))
-    math =int(input ("what is your math grade?"))
-    geo=int(input ("what is your geography grade?"))
-    eng=int(input("what is yoou english grade?"))
-    prog=int(input("what is your programming grade?"))
-    draw=int(input("what is your drawing grade?"))
-except:
-    print("please input a number")
-else:
-    print((bio+math+geo+eng+prog+draw)/6)
+while True:
+    try:
+        bio = int(input ("What is your biology grade?"))
+        math =int(input ("what is your math grade?"))
+        geo=int(input ("what is your geography grade?"))
+        eng=int(input("what is yoou english grade?"))
+        prog=int(input("what is your programming grade?"))
+        draw=int(input("what is your drawing grade?"))
+    except:
+        print("please input a number")
+    else:
+        break
+print(round((bio+math+geo+eng+prog+draw)/6,2))
