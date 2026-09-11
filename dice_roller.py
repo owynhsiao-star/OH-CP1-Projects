8adcb4ab53d98fde9cb9bda754459d6f2add9dac
Pneumonoultramicroscopicsilicovolcanoconiosis <owyn.hsiao@ucas-edu.net>
2026-09-11 01:26:28 +0000

    dice_roller_assignment

--- dice_roller.py
+++ dice_roller.py
@@ -1,30 +1,35 @@
-#OH, dice roller assignment
+#OH, 1st period, dice roller assignment
 import random
 while True:
-        answer=int(input("what dice would you like: "))
-        if answer==4:
-            dice=random.randint(1,answer)    
-            print(f"You rolled " , dice)
-            break
-        if answer==6:
-            dice=random.randint(1,answer)    
-            print(f"You rolled " , dice)
-            break
-        if answer==8:
-            dice=random.randint(1,answer)    
-            print(f"You rolled " , dice)
-            break
-        if answer==10:
-            dice=random.randint(1,answer)    
-            print(f"You rolled " , dice)
-            break
-        if answer==12:
-            dice=random.randint(1,answer)    
-            print(f"You rolled " , dice)
-            break
-        if answer==20:
-            dice=random.randint(1,answer)    
-            print(f"You rolled " , dice)
-            break
-        else:
-             print(f"Please input 4, 6, 8, 10, 12, or 20")
+    try:
+        while True:
+                answer=int(input("what dice would you like: "))
+                if answer==4:
+                    dice=random.randint(1,answer)    
+
+                    break
+                if answer==6:
+                    dice=random.randint(1,answer)    
+
+                    break
+                if answer==8:
+                    dice=random.randint(1,answer)    
+
+                    break
+                if answer==10:
+                    dice=random.randint(1,answer)    
+
+                    break
+                if answer==12:
+                    dice=random.randint(1,answer)    
+                    break
+                if answer==20:
+                    dice=random.randint(1,answer)    
+                    break
+                else:
+                    print(f"Please input 4, 6, 8, 10, 12, or 20")
+    except:
+        print("No characters besides numbers")
+    else:
+        print("You rolled" , dice)
+        break
