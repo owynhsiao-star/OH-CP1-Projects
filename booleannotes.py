9b5f4ff93c656e6ca9cd841b0c969eca8c47aba9
owynhsiao-star <owyn.hsiao@ucas-edu.net>
2026-09-23 15:27:47 +0000

    Boolean notes

---
+++ booleannotes.py
@@ -0,0 +1,5 @@
+#boolean: a data type that can be true or false
+#comparison operators: Symbols used to compare booleans(<> == >= <= != )
+#bool() converts a value into a boolean
+#simple data types: Building blocks for data types, integer float string and boolean
+#booleans are most often used to control the flow of a program _ decides wether or not a condition is mer so the program can decide things
