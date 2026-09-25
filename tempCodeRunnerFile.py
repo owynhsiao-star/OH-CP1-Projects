f26bdf5171371d194abc02152802b812a53c6bdb
owynhsiao-star <owyn.hsiao@ucas-edu.net>
2026-09-25 09:20:53 -0600

    Undefined proceedingsfixed

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,21 @@
-Obadielpneumonoultramicroscopicsilicavolcanoconiosiszophander
+#OH, period 1, usersignin assignment
+username="I'm groovy and never glooby"
+pasword="Beegee"
+ask=input("What is you username? ")
+aske=input("What is you password? ")
+if ask==username:
+    proceedings=True
+    print("Username is correct")
+else:
+    print("Ah ah ah, thats not it, no siree bob")
+if aske==pasword:
+    proceedenings=True
+    print("Password is correct")
+else:
+    print("Learn your own password")
+if bool(proceedings)==True and bool(proceedenings)==True:
+    print("Welcome " + username)
+else:
+    print("NOT welcome")
+
+
--- user_signin.py
+++ user_signin.py
@@ -1,20 +1,18 @@
 #OH, period 1, usersignin assignment
-username="I'm groovy and never glooby"
+username="Micheal Jordan"
 pasword="Beegee"
 ask=input("What is you username? ")
 aske=input("What is you password? ")
 if ask==username:
-    proceedings=True
     print("Username is correct")
 else:
     print("Ah ah ah, thats not it, no siree bob")
 if aske==pasword:
-    proceedenings=True
     print("Password is correct")
 else:
     print("Learn your own password")
-if bool(proceedings)==True and bool(proceedenings)==True:
-    print("Welcome " + username)
+if ask==username and aske == pasword:
+    print("Welcome to the program " + username+"!")
 else:
     print("NOT welcome")
 
